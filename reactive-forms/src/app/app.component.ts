de713dca6a2ee62b9dfbb247979c3b2b92fc5e29
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-07-30 20:13:11 +0530

    Finish forms

--- reactive-forms/src/app/app.component.ts
+++ reactive-forms/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FormControl, ReactiveFormsModule, Validators} from "@angular/forms";
 import {ReactiveFormComponent} from "./reactive-form/reactive-form.component";
+import {DanAmaruiComponent} from "./dan-amarui/dan-amarui.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule, ReactiveFormComponent],
+  imports: [RouterOutlet, ReactiveFormsModule, ReactiveFormComponent, DanAmaruiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

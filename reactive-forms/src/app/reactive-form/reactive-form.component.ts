a842dea033a2b51e3c76bf4845e1e7b24cab4fb3
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-07-30 19:00:21 +0530

    Add reactive forms

---
+++ reactive-forms/src/app/reactive-form/reactive-form.component.ts
@@ -0,0 +1,62 @@
+import {Component} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgClass} from "@angular/common";
+
+@Component({
+  selector: 'app-reactive-form',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    NgClass
+  ],
+  templateUrl: './reactive-form.component.html',
+  styleUrl: './reactive-form.component.css'
+})
+export class ReactiveFormComponent {
+  frm = new FormGroup({
+    id: new FormControl('',
+      [
+        Validators.required,
+        Validators.pattern(/^C\d{3}$/)
+      ]),
+    name: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^[A-Za-z ]+$/),
+      Validators.minLength(3)
+    ]),
+    contact: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^\d{3}-\d{7}$/)
+    ])
+  });
+
+  get id(){
+    return this.frm.get('id')!;
+  }
+
+  get name(){
+    return this.frm.get('name')!;
+  }
+
+  get contact(){
+    return this.frm.get('contact')!;
+  }
+
+  getCSSClasses(control: AbstractControl){
+    return {
+      'is-valid': control.touched && control.valid,
+      'is-invalid': control.touched && control.invalid
+    };
+  }
+
+  save() {
+    this.frm.markAllAsTouched();
+    if (this.frm.valid){
+      alert("Valid");
+      console.log(this.frm.value);
+      this.frm.reset();
+    }else{
+      alert("Invalid");
+    }
+  }
+}

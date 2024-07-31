f156dc7f9a94dfcd188a23b66a430df735bfdee2
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-07-31 11:36:55 +0530

    Prepare reactive form model

--- dynamic-forms/src/app/app.component.ts
+++ dynamic-forms/src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
+  private fb = inject(FormBuilder);
+  form = this.fb.group({
+    nic: [],
+    name: [],
+    gender: [],
+    subjects: this.fb.array([[]]),
+    marks: this.fb.array([[]])
+  });
 }

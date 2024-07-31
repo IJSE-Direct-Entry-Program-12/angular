b0dfefe59b8e6fb3553e2a1f9f25316ddda43bc9
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-07-31 13:36:19 +0530

    Finish report card logic

--- dynamic-forms/src/app/report-card/report-card.component.ts
+++ dynamic-forms/src/app/report-card/report-card.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+
+type Student = {
+  name: string,
+  nic: string,
+  subjects: Array<{name: string, marks: number}>
+}
 
 @Component({
   selector: 'app-report-card',
@@ -8,5 +14,28 @@ import { Component } from '@angular/core';
   styleUrl: './report-card.component.css'
 })
 export class ReportCardComponent {
+  @Input()
+  student!: Student;
 
+  getTotalMarks() {
+    let totalMarks = 0;
+    this.student.subjects.forEach(
+      subject =>
+        totalMarks += subject.marks);
+    return totalMarks;
+  }
+
+  getAvgMarks() {
+    return this.getTotalMarks() / this.student.subjects.length;
+  }
+
+  getGrade() {
+    const avgMarks = this.getAvgMarks();
+    if (avgMarks >= 90) return "A+";
+    if (avgMarks >= 75) return "A";
+    if (avgMarks >= 65) return "B";
+    if (avgMarks >= 55) return "C";
+    if (avgMarks >= 45) return "S";
+    return "F";
+  }
 }

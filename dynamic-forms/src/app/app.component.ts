f7446f2158050f9edc69bc2d2ab72f36592e5e4c
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-07-31 12:37:26 +0530

    Revamp form model

--- dynamic-forms/src/app/app.component.ts
+++ dynamic-forms/src/app/app.component.ts
@@ -15,28 +15,32 @@ export class AppComponent {
     nic: ['', [Validators.required,
       Validators.pattern(/^\d{9}[Vv]$/)]],
     name: ['', [Validators.required,
-    Validators.pattern(/^[A-Za-z ]+$/)]],
+      Validators.pattern(/^[A-Za-z ]+$/)]],
     gender: ['', [Validators.required]],
     subjects: this.fb.array([
-      this.fb.group({
-        name: [''],
-        marks: ['']
-      }),
-      this.fb.group({
-        name: [''],
-        marks: ['']
-      }),
-      this.fb.group({
-        name: [''],
-        marks: ['']
-      })
+      this.createSubjectGroup(),
+      this.createSubjectGroup(),
+      this.createSubjectGroup()
     ])
   });
 
+  getCssClasses(formControlName: string) {
+    const control =
+      this.form.get(formControlName)!;
+    return {
+      'is-invalid': control.touched && control.invalid,
+      'is-valid': control.touched && control.valid
+    }
+  }
+
   addNewSubject() {
-    this.form.controls.subjects.push(this.fb.group({
+    this.form.controls.subjects.push(this.createSubjectGroup());
+  }
+
+  createSubjectGroup(){
+    return this.fb.group({
       name: [''],
       marks: ['']
-    }))
+    });
   }
 }

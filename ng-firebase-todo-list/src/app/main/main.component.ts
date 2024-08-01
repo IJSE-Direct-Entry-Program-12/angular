4e82090b4fbf8571d0c6fcfb125746c700500fc2
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-08-01 17:28:03 +0530

    Add to-do list app

--- ng-firebase-todo-list/src/app/main/main.component.ts
+++ ng-firebase-todo-list/src/app/main/main.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import {Title} from "@angular/platform-browser";
+import {AppBarComponent} from "../app-bar/app-bar.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [
+    AppBarComponent
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
